77c5da5efd72bf24afffcb726ae0fbe3aee632e6
Shaaraty <Shaaraty0906@gmail.com>
2024-05-21 22:49:14 +0800

    Change font size in line number 107

--- Menu.py
+++ Menu.py
@@ -104,7 +104,7 @@ def main_menu():
         PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250), 
                             text_input="Play", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
         OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400), 
-                            text_input="Options", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
+                            text_input="Options", font=get_font(51), base_color="#d7fcd4", hovering_color="White")
         QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550), 
                             text_input="Quit", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
 
