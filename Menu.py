2ad710d4aeee1e3637e3d32a643fd936bc2bbc49
Shaaraty <Shaaraty0906@gmail.com>
2024-06-06 12:12:32 +0800

    change line number 120

--- Menu.py
+++ Menu.py
@@ -117,7 +117,7 @@ def main_menu():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
                     play()
-                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                if INSTRUCTION_BUTTON.checkForInput(MENU_MOUSE_POS):
                     options()
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
