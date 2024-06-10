b363403554ffe3bf0b9f67af1cf81a4c8cdd4c6a
Anantha Mukesh <anantha2706@gmail.com>
2024-06-10 09:42:20 +0800

    added line 182 so that user can type answer in the input box

--- medium.py
+++ medium.py
@@ -179,7 +179,8 @@ def medium_level():
         pygame.draw.rect(screen, color, input_box, 2)
         user_input_text = FONT.render(user_guess, True, (0, 0, 0))
         screen.blit(prompt_text, (SCREEN_SIZE // 2 - prompt_text.get_width() // 2, SCREEN_SIZE // 2 - 50))               
-
+        screen.blit(user_input_text, (input_box.x + 5, input_box.y + 5))
+        
                              
                          
     pygame.quit()
