9c0163ddb2571bccb2578aab697ba67c9fa82cf4
Shaaraty <Shaaraty0906@gmail.com>
2024-06-20 04:52:56 +0800

    delete change colour

--- Menu.py
+++ Menu.py
@@ -35,13 +35,7 @@ class Button():
 		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
 			return True
 		return False
-
-	def changeColor(self, position):
-		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-			self.text = self.font.render(self.text_input, True, self.hovering_color)
-		else:
-			self.text = self.font.render(self.text_input, True, self.base_color)
-
+     
 def play():
     while True:
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
