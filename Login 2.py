c78dfb3681fcc426d6d0cd9371c187932122c1df
vishallan <vishallanramamurthi13@gmail.com>
2024-06-20 11:32:15 +0800

    colours

--- Login 2.py
+++ Login 2.py
@@ -7,3 +7,8 @@ pygame.init()
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Login and Registration System")
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
