e456716952a1f5d14f5089eacb7bf017b0b088b4
vishallan <vishallanramamurthi13@gmail.com>
2024-06-20 12:58:48 +0800

    Leaderboard class stores time records for players

--- leaderboard.py
+++ leaderboard.py
@@ -7,3 +7,10 @@ pygame.init()
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = (200, 200, 200)
+
+class Leaderboard:
+    def __init__(self):
+        self.player_times = {
+            "player1": {"level1": 30, "level2": 45, "level3": 60},
+            "player2": {"level1": 40, "level2": 50, "level3": 70},
+            "player3": {"level1": 35, "level2": 55, "level3": 65},
