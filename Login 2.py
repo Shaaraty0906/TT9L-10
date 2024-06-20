67648ca737c84df1dccd67422a3bb385c037cfa3
vishallan <vishallanramamurthi13@gmail.com>
2024-06-20 11:28:42 +0800

    setting for the screen

--- Login 2.py
+++ Login 2.py
@@ -2,3 +2,8 @@ import pygame
 import sys
 
 pygame.init() 
+
+# setting for screen
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Login and Registration System")
