b5cfd8b88fad6fd9ebc308dcffc950c4a891180e
vishallan <vishallanramamurthi13@gmail.com>
2024-06-20 13:04:04 +0800

    record time for recording time taken by players to complete a level

--- leaderboard.py
+++ leaderboard.py
@@ -14,3 +14,11 @@ class Leaderboard:
             "player1": {"level1": 30, "level2": 45, "level3": 60},
             "player2": {"level1": 40, "level2": 50, "level3": 70},
             "player3": {"level1": 35, "level2": 55, "level3": 65},
+        }
+
+    def record_time(self, player_name, level):
+        start_time = time.time()
+        time.sleep(2)  # This would be your game logic
+        end_time = time.time()
+        time_taken = end_time - start_time
+        self.player_times[player_name][level] = time_taken
